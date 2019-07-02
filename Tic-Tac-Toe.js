967c0261f1d14f96e44295c3fd2cd85b45b0102e
imunirah <moneerah.md@gmail.com>
2019-07-02 14:02:47 +0300

    v2

--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -42,14 +42,14 @@ let move = 1;
 $('.square').one('click',function (){
   if ((move%2) ==1){
    
-    //$(".square").off("click");
+    // $(".square").off("click");
       $(this).append('x');
-      //$(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
+    //   $(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
   } else {
     //console.log("o working !!"); 
     //$(".square").off("click");
     $(this).append('o');
-    //$(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
+    // $(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
   }
   move++;
   checkWinner();
@@ -74,37 +74,37 @@ var squares = [];
 
 function checkWinner(){
     for (var i=1; i<=9; i++){
-squares[i]= document.getElementById ('A'+i).innerHTML; 
+squares[i]= document.getElementById ('A'+ i).innerHTML; 
 }
 // columns 
 if(squares[1]==squares[2] && squares[1]==squares[3]&& squares[1]!=""){
   
-  alert('player'+ squares[1] +' wins !');
+  alert('player' + squares[1] + ' wins !');
 }
    
 if(squares[4]==squares[5] && squares[4]==squares[6]&& squares[4]!=="")
-    alert ('player'+ squares[4] +' wins !');
+    alert ('player' + squares[4] + ' wins !');
     
 if(squares[7]==squares[8] && squares[7]==squares[9]&& squares[7]!=="")
-    alert('player'+ squares[7] +' wins !');
+    alert('player' + squares[7] + ' wins !');
    
 
     //rows 
 if(squares[1]==squares[4] && squares[1]==squares[7]&& squares[1]!="")
-    alert ('player'+ squares[1] +' wins !');
+    alert ('player' + squares[1] + ' wins !');
    
 if(squares[2]==squares[5] && squares[2]==squares[8]&& squares[2]!=="")
-    alert('player'+ squares[2] +' wins !');
+    alert('player' + squares[2] + ' wins !');
     
 if(squares[3]==squares[6] && squares[3]==squares[9]&& squares[3]!=="")
-    console.log ('player'+ squares[3] +' wins !');
+    console.log ('player' + squares[3] + ' wins !');
  
     //diagonal 
 if(squares[1]==squares[5] && squares[1]==squares[9]&& squares[1]!="")
-    alert('player'+ squares[1] +' wins !');
+    alert('player' + squares[1] + ' wins !');
    
 if(squares[3]==squares[5] && squares[3]==squares[7]&& squares[3]!=="")
-    alert('player'+ squares[3] +'wins !');
+    alert('player' + squares[3] + 'wins !');
     
     
 }

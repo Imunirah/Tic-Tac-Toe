e68adcc7511b6b0d91223277f4b28f576452b995
imunirah <moneerah.md@gmail.com>
2019-07-03 23:54:30 +0300

    v3

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
+
 # Tic-Tac-Toe
+
+
+## Overview:
+
+
+
+Create a game is the first individual project in software engineering course, in the end of this project I will be abel to use all tolls that we leanerd during the last weeks.
+
+
+
+---
+
+### List technologies used:
+
+- **HTML**
+- **CSS**
+- **JavaScript**
+- **jQuery**
+-  **Xmind website**
+-  **Balsamiq website**
+
+
+---
+
+### Wireframes:
+
+http://github.com - automatic!
+[GitHub](https://balsamiq.cloud/sz9cutq/pwgooyl)
+
+
+
+---
+
+### Plan:
+
+
+Winter is the coldest time of the year.  The days are shorter and the nights are longer and often families spend their time on indoor areas and enjoying doing some different activities. As a developer, I thought is very important to create a traditional fun game that depends on the challenge between players, I decided to create a Tic-Tac-Toe game winter version that helps families to enjoy their time in winter, on the other hand; I can improve my skills in Html, Css and Javascript.
+
+### Tic-Tac-Toe plan:
+- I have analyses project requirements then make a working plan for more details please check the attachment file "Tic-Tac-Toe plan.xmind ".
+- Html part has one main div that includes one class called "first" this div includes three divs with class name " title, bg,
+the game", "game" class  include three divs called row. Also it is cove heads, body and button.
+- JavaScript part has an append function that allows adding X and O, click funcation for player move and Checkwinner function that compares all scores then show the winner.
+- Css part changes all styles/layout of the page.
+
+
+
+### User Stories
+
+- User able to start a new tic tac toe game.
+- User not able to select X or O in this stage.
+- User will receive a box massage if he/she win or lose.
+- User not able to click the same square twice.
+- User must not be able to play the game again without refreshing the page.
+
+
+###  List unsolved problems:
+- Allow the user to choose between X and O.
+- Responsive website.
+
+
+### How you solved for the winner:
+- I created a function that compares all the results that may the player get it.
+- I created an array that includes all the results.
+- l created a condition that includes the required condition to be the winner.
+
+
+
+
+
--- Tic-Tac-Toe.js
+++ Tic-Tac-Toe.js
@@ -1,40 +1,5 @@
-// $(document).ready( function() {
-//     $("button").click( function(){
-//     $("p").hide(1000,function(){$("span").show()});
-//     });
-//     });
 
 
-// const div = document.querySelector('div');
-// heading.style.color = 'pink';
-// heading.style.background = 'blue';
-// heading.style.fontSize = '100px';
-
-//this for the font 
-// $(document).ready( function() {
-// $("button").click( function(){
-// $ ("div.wrapper").css({"color":"#00F"});
-// })
-// });
-
-// var playBoard;
-// const playerOne= "X"; 
-// const playerTwo= "O";
-// const winCombos = [
-// [0,1,2],
-// [3,4,5],
-// [6,7,8],
-// [0,4,8],
-// [2,4,6],
-// [1,4,7],
-// [0,4,8],
-// [6,4,2]
-
-// const square = document.querySelectorAll('.square');
-
-
-
-  
 
 let move = 1; 
 
@@ -42,39 +7,23 @@ let move = 1;
 $('.square').one('click',function (){
   if ((move%2) ==1){
    
-    // $(".square").off("click");
-      $(this).append('x');
-    //   $(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
+   $(this).append('x');
   } else {
-    //console.log("o working !!"); 
-    //$(".square").off("click");
+   
     $(this).append('o');
-    // $(body).innerHTML('<button onclick="location.reload();"id="reload">Play Again</button>');
   }
   move++;
   checkWinner();
   
 }); 
 
-var turn= true;
-var squares = [];
+
+var squares = []; // the array includes all results that players may face it 
 
 
-// function reset(X,Y,Z){
-//     document.getElementById('A'+X).style.color= "#000";
-//     document.getElementById('A'+Y).style.color= "#000";
-//     document.getElementById('A'+Z).style.color= "#000";
-    
-//     if (checkWinner()= true) {
-//     setTimeout(function(){location.reload()},2000); }
-        
-//     }
-
-
-
-function checkWinner(){
-    for (var i=1; i<=9; i++){
-squares[i]= document.getElementById ('A'+ i).innerHTML; 
+function checkWinner(){ // take all squares and compare them "share an example ""
+    for (var i=1; i<=9; i++){  //condition
+squares[i]= document.getElementById ('A'+ i).innerHTML;  // put in each single array the its html  
 }
 // columns 
 if(squares[1]==squares[2] && squares[1]==squares[3]&& squares[1]!=""){
@@ -97,7 +46,7 @@ if(squares[2]==squares[5] && squares[2]==squares[8]&& squares[2]!=="")
     alert(' player ' + squares[2] + ' wins ! ');
     
 if(squares[3]==squares[6] && squares[3]==squares[9]&& squares[3]!=="")
-    console.log (' player ' + squares[3] + ' wins ! ');
+    alert(' player ' + squares[3] + ' wins ! ');
  
     //diagonal 
 if(squares[1]==squares[5] && squares[1]==squares[9]&& squares[1]!="")
@@ -115,18 +64,6 @@ if(squares[3]==squares[5] && squares[3]==squares[7]&& squares[3]!=="")
    
     
 
-// function insert(id) {
-// var s = document.getElementById(id);
-// if(turn && s.innerHTML==""){
-//     s.innerHTML="x";
-//     turn = !turn;
-
-// }else (!turn && s.innerHTML=="")
-// s.innerHTML="o";
-//     turn = !turn;
-// document.getElementById(id).innerHTML="0";
-// turn = !turn;
-// }
 
 
 
@@ -139,31 +76,3 @@ if(squares[3]==squares[5] && squares[3]==squares[7]&& squares[3]!=="")
 
 
 
-//  let square1 =$("#1").text;
-//  let square2= $("#2").text; 
-//  let square3= $("#3").text;
-//  let square4= $("#4").text;
-//  let square5= $("#5").text;
-//  let square6= $("#6").text;
-//  let square7= $("#7").text;
-//  let square8= $("#8").text;
-//  let square9= $("#9").text;
-//  console.log(square1); 
-
-
-
-
- //}
-
-//alert("cong");
-
-//}
-
-
-
-
-// var hasWinner = 0, moveCount=0; //finding winners ,the total number of moves
-
-// function boardMsg(x){
-//     return $("#board").text(x);
-// 
